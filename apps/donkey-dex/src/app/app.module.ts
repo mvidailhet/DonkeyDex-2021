d41d5b5643c398764cc0f56a3bae7a62eb00691f
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-02-08 15:41:24 +0100

    Created pokemons list Fetch pokemons from api and show name in template

--- apps/donkey-dex/src/app/app.module.ts
+++ apps/donkey-dex/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { PokemonsListComponent } from './pokemons-list/pokemons-list.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, PokemonsListComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
