af870e0fc1f6a9124fe6c5154e856a399532d0f5
Michel Vidailhet <michel.vidailhet@gmail.com>
2022-02-08 12:01:28 +0100

    Created project : npx create-nx-workspace@latest DonkeyDex Base Doc : https://nx.dev/angular-tutorial/01-create-application

---
+++ apps/donkey-dex/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'donkey-dex-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'donkey-dex';
+}
